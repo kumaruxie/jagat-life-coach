5cf263659cd61c790fc9f3f40f6cdd64672bc535
kumaruxie <kumarworkstation@gmail.com>
2026-05-17 19:41:24 +0530

    refined design and updated achievements section

--- vite.config.js
+++ vite.config.js
@@ -6,23 +6,6 @@ export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
-    chunkSizeWarningLimit: 2000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('@splinetool/react-spline') || id.includes('@splinetool/runtime')) {
-              return 'spline';
-            }
-            if (id.includes('framer-motion')) {
-              return 'framer';
-            }
-            if (id.includes('react/') || id.includes('react-dom/') || id.includes('scheduler/')) {
-              return 'react-vendor';
-            }
-          }
-        }
-      }
-    }
+    chunkSizeWarningLimit: 2000
   }
 })
